e61bf1a04bfa1d126b856748c57ea17b8b95b360
Glen Bray <glen.bray@gmail.com>
2020-03-11 23:15:52 +1100

    init commit

--- spec/elastic_parser_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe ElasticParser do
-  it "has a version number" do
-    expect(ElasticParser::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
